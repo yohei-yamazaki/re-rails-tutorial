15cd52c7b08da4d3103b9d15eb4c10e3ad7a636c
yamazaki_yohei <y_yamazaki@timedia.co.jp>
2021-04-01 18:06:57 +0900

    helloとgoodbyeのactionを追加し、goodbyeを表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+# Apppliction controller class
 class ApplicationController < ActionController::Base
+  def hello
+    render html: '¡Hola, mundo!'
+  end
+
+  def goodbye
+    render html: 'goodbye, world!'
+  end
 end
